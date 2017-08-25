f8ad5080d4105674e3b377e129e780a24eeea19d
svetulek <svetulek@svetuleks-MacBook-Air.local>
2017-08-24 17:01:24 -0700

    Hashmap

--- HomeworkLevel2Class6/hashmap.java
+++ HomeworkLevel2Class6/hashmap.java
@@ -28,31 +28,31 @@ public class hashmap {
 					wb.get("http://qabidder.net/testwave/#/page/login");
 					
 				
-					//Thread.sleep(2000);
+					
 					WebElement usernameTextBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("exampleInputEmail1")));//wb.findElement(By.id("exampleInputEmail1"));
 					usernameTextBox.sendKeys(values.get("email"));
 					WebElement passwordTextBox = wb.findElement(By.id("exampleInputPassword1"));
-					passwordTextBox.sendKeys(values.get("Password"));
+					passwordTextBox.sendKeys(values.get("password"));
 					wb.findElement(By.name("login.loginForm")).submit();
-					Thread.sleep(5000);
+				
 					WebElement reports = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("html/body/div[2]/aside[1]/div/nav/ul/li[4]/a/span")));
 					//WebElement reports = wb.findElement(By.xpath ("html/body/div[2]/aside[1]/div/nav/ul/li[4]/a/span"));
 					reports.click();
-					//Thread.sleep(10000);
+					
 					WebElement InfoTab = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("html/body/div[2]/section/div/div/form/div[1]/div[2]/ol/li[2]/h4/div")));
 					InfoTab.click();
-					//Thread.sleep(10000);
+					
 					WebElement LastName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("html/body/div[2]/section/div/div/form/div[1]/div[2]/div[3]/fieldset/div[2]/div[1]/div/div/input")));
-					LastName.sendKeys(values.get("Smith"));
+					LastName.sendKeys(values.get("FirstName"));
 					WebElement FirstName = wb.findElement(By.xpath("html/body/div[2]/section/div/div/form/div[1]/div[2]/div[3]/fieldset/div[2]/div[2]/div/div/input"));
-					FirstName.sendKeys(values.get("Simon"));
+					FirstName.sendKeys(values.get("LastName"));
 							;
 				
 				    WebElement GMembership = wb.findElementByXPath("html/body/div[2]/section/div/div/form/div[1]/div[2]/ol/li[5]/h4/div");
 					GMembership.click();
-					//Thread.sleep(10000);
+					
 					WebElement LocationofStop = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("locationOfStop")));
-					LocationofStop.sendKeys(values.get("San Jose"));
+					LocationofStop.sendKeys(values.get("LocationofStop"));
 							
 					WebElement Preview = wb.findElementByXPath("html/body/div[2]/section/div[1]/div/form/div[1]/div[2]/ol/li[8]/h4/div");     
 					Preview.click();
@@ -60,7 +60,7 @@ public class hashmap {
 					WebElement Publish = wb.findElementByXPath("html/body/div[2]/section/div[1]/div/form/div[2]/button[1]");
 					Publish.click();
 					
-					//Thread.sleep(10000);
+					
 					WebElement Popup = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='ngdialog1']/div[2]/div/div/button[1]")));				
 					Popup.click();
 		}
